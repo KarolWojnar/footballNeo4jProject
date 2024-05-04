be6a6157f2114f81bb6fc5836d17d920c4c9c521
KarolWojnar <karolwojnar47@gmail.com>
2024-05-04 14:12:34 +0200

    note -> team

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ def importTeams2neo4j(filePath):
         for line in file:
             data = line.strip().split(';')
             teamId = int(data[0])
-            node = Node("Team",
+            team = Node("Team",
                         id=teamId, nazwa=data[1], sezon_rozgrywek=data[2],
-                        gole_strzelone_w_domu=data[3], gole_strzelone_na_wyjezdzie=data[4],
-                        gole_stracone_w_domu=data[5], gole_stracone_na_wyjezdzie=data[6],
-                        zolte_kartki=data[7], czerwone_kartki=data[8], mecze_domowe=data[9],
-                        mecze_na_wyjezdzie=data[10], srednia_goli_straconych_w_domu=data[11],
-                        srednia_goli_strzelonych_w_domu=data[12], srednia_goli_straconych_na_wyjezdzie=data[13],
-                        srednia_goli_strzelonych_na_wyjezdzie=data[14], wygrane_w_domu=data[15],
-                        wygrane_na_wyjezdzie=data[16], przegrane_w_domu=data[17],
-                        przegrane_na_wyjezdzie=data[18], remisy_w_domu=data[19],
-                        remisy_na_wyjezdzie=data[20])
-            graph.create(node)
+                        gole_strzelone_w_domu=float(data[3]), gole_strzelone_na_wyjezdzie=float(data[4]),
+                        gole_stracone_w_domu=float(data[5]), gole_stracone_na_wyjezdzie=float(data[6]),
+                        zolte_kartki=float(data[7]), czerwone_kartki=float(data[8]), mecze_domowe=float(data[9]),
+                        mecze_na_wyjezdzie=float(data[10]), srednia_goli_straconych_w_domu=float(data[11]),
+                        srednia_goli_strzelonych_w_domu=float(data[12]), srednia_goli_straconych_na_wyjezdzie=float(data[13]),
+                        srednia_goli_strzelonych_na_wyjezdzie=float(data[14]), wygrane_w_domu=float(data[15]),
+                        wygrane_na_wyjezdzie=float(data[16]), przegrane_w_domu=float(data[17]),
+                        przegrane_na_wyjezdzie=float(data[18]), remisy_w_domu=float(data[19]),
+                        remisy_na_wyjezdzie=float(data[20]))
+            graph.create(team)
 
 def importPlayers2neo4j(filePath):
     with open(filePath, 'r', encoding='utf-8') as file:
@@ -29,15 +29,15 @@ def importPlayers2neo4j(filePath):
             playerId = int(data[0])
             player = Node("Player",
                         id=playerId, fisrt_name=data[1], last_name=data[2],
-                        season=data[4], age=data[5], height=data[6],
-                        weight=data[7], country=data[8], appearances=data[9],
-                        minutes=data[10], position=data[11], rating=data[12],
-                        shots=data[13], shots_on_target=data[14], goals=data[15],
-                        passes=data[16],  pass_accuracy=data[17],
-                        key_passes=data[18], assists=data[19], duels=data[20],
-                        duels_won=data[21], interceptions=data[22], successful_interceptions=data[23],
-                        dribbles=data[24], successful_dribbles=data[25], fouls_drawn=data[26],
-                        fouls_committed=data[27], yellow_cards=data[28], red_cards=data[29],
+                        season=data[4], age=float(data[5]), height=float(data[6]),
+                        weight=float(data[7]), country=data[8], appearances=float(data[9]),
+                        minutes=float(data[10]), position=data[11], rating=float(data[12]),
+                        shots=float(data[13]), shots_on_target=float(data[14]), goals=float(data[15]),
+                        passes=float(data[16]),  pass_accuracy=float(data[17]),
+                        key_passes=float(data[18]), assists=float(data[19]), duels=float(data[20]),
+                        duels_won=float(data[21]), interceptions=float(data[22]), successful_interceptions=float(data[23]),
+                        dribbles=float(data[24]), successful_dribbles=float(data[25]), fouls_drawn=float(data[26]),
+                        fouls_committed=float(data[27]), yellow_cards=float(data[28]), red_cards=float(data[29]),
                         injured=data[30])
             graph.create(player)
 
